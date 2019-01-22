1f7b4d0071aa8fe949766ff830f1988563761217
Nils Macke <nilsmacke@outlook.de>
2019-01-22 21:10:45 +0100

    Added necessary Attributes

---
+++ LinqToCsv/LinqToCsv/Attributes/CsvFileInfoAttribute.cs
@@ -0,0 +1,78 @@
+﻿using LinqToCsv.Csv;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqToCsv.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+	public class CsvFileInfoAttribute: Attribute
+	{
+		private CsvSettings _csvSettings;
+
+		public bool HasSeparator
+		{
+			get
+			{
+				return this._csvSettings.HasSeparator;
+			}
+			set
+			{
+				this.HasSeparator = value;
+			}
+		}
+
+		public char Separator
+		{
+			get
+			{
+				return this._csvSettings.Separator;
+			}
+			set
+			{
+				this._csvSettings.Separator = value;
+			}
+		}
+
+		public char Delimiter
+		{
+			get
+			{
+				return this._csvSettings.Delimiter;
+			}
+			set
+			{
+				this._csvSettings.Delimiter = value;
+			}
+		}
+
+		public bool HasColumnHeaders
+		{
+			get
+			{
+				return this._csvSettings.HasColumnHeaders;
+			}
+			set
+			{
+				this._csvSettings.HasColumnHeaders = value;
+			}
+		}
+
+		public CsvFileInfoAttribute()
+		{
+			this._csvSettings = CsvSettings.Default;
+		}
+		public CsvFileInfoAttribute(char separator, char delimiter)
+			:this()
+		{
+			this.HasSeparator = true;
+			this.Separator = separator;
+			this.Delimiter = delimiter;
+		}
+
+		public CsvSettings GetSettings()
+		{
+			return this._csvSettings;
+		}
+	}
+}
---
+++ LinqToCsv/LinqToCsv/Csv/CsvSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqToCsv.Csv
+{
+	public class CsvSettings
+	{
+		public static CsvSettings Default
+		{
+			get
+			{
+				return new CsvSettings(';', '"', false);
+			}
+		}
+
+		public bool HasSeparator { get; set; }
+
+		public char Separator { get; set; }
+
+		public char Delimiter { get; set; }
+
+		public bool HasColumnHeaders { get; set; }
+
+		public CsvSettings(char separator, char delimiter, bool hasColumnHeaders)
+		{
+			this.Delimiter = delimiter;
+			this.HasColumnHeaders = hasColumnHeaders;
+			this.Separator = separator;
+		}
+	}
+}
